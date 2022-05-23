6029769944d9ead2ba868b5c99175934de8cbeeb
Dongvin,Park <dongvin99@naver.com>
2022-05-23 14:33:40 +0900

    Initial Setup.

---
+++ demo 6/src/main/java/AloneTodoApp/demo/AloneTodoappApplication.java
@@ -0,0 +1,13 @@
+package AloneTodoApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneTodoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AloneTodoappApplication.class, args);
+	}
+
+}
