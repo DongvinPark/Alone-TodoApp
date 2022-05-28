8c5ed8c9649c0f5620cd5a23d6ca5bde38a8d80c
Dongvin,Park <dongvin99@naver.com>
2022-05-28 15:26:43 +0900

    Refactoring TestTodoController

--- demo 6/src/main/java/AloneTodoApp/demo/controller/TestTodoController.java
+++ demo 6/src/main/java/AloneTodoApp/demo/controller/TestTodoController.java
@@ -43,19 +43,7 @@ public class TestTodoController {
 
         List<TodoDTO> dtos = toDoEntities.stream().map(TodoDTO::new).collect(Collectors.toList());
 
-        for(TodoDTO dto : dtos){
-            for(TodoReplyEntity todoReplyEntity : replyEntities){
-
-                log.info("Todo 아이디 : " + dto.getId());
-                log.info("TodoReply의 parentTodo 아이디 : " + todoReplyEntity.getParentTodoId());
-
-                if(dto.getId().equals(todoReplyEntity.getParentTodoId())){
-                    log.info("todoDTO에 리플라이 넣음");
-                    log.info(todoReplyEntity.getParentTodoId());
-                    dto.getReplies().add(todoReplyEntity.getTitle());
-                }
-            }//inner for
-        }//outer for
+        extracted(replyEntities, dtos);
 
         ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
@@ -74,15 +62,17 @@ public class TestTodoController {
 
             entity.setUserId(tempUserId);
 
-
-            List<TodoEntity> entities = service.create(entity);//TodoServece 쪽의 create 메서드를 수정해야 한다.
+            List<TodoEntity> toDoentities = service.create(entity);//TodoServece 쪽의 create 메서드를 수정해야 한다.
             //이 메서드에서 todo 엔티티 하나 찾을 때마다, 그 엔티티에 대응되는 리플라이들도 붙여서 반환해야 하기 때문이다.
             //그런데, 어차피 서비스 계층에서는 리포지토리가 기본제공하는 메서드밖에 사용하지 못한다.
             //즉, 여기에서 List<TodoEntity> entities 가 만들어질 때, 중간에 리플라이들을 각 Todo 엔티티의 고유 아이디에 맞는 것으로 선별해서 맵핑해주는 작업을 한 수가 없다는 뜻이다.
 
+            List<TodoReplyEntity> replyEntities = replyService.retrieveReplies(tempUserId);
 
             //그 맵핑 작업은 결국 여기서 해야 한다. 왜냐면 엔티티의 리스트를 그냥 반환하는 것이 아니라 여기에서 추가로 리프라이 리스트를 만들어서 반환해야 하니까.
-            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            List<TodoDTO> dtos = toDoentities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            extracted(replyEntities, dtos);
 
             ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
 
@@ -99,18 +89,17 @@ public class TestTodoController {
     //그러면 이놈은 뭘 반환해야 하지?
     // 현재 만든 리플라이만 반환하는 것이 아니라, 그 리플라이를 포함하고 있는 부모 todo 엔티티, 그리고 다른 todo 엔티티도 모두 같이 반환해야 하지 않나?
     //결과적으로 이놈은 현재 TestTodoController클래스의 getTodoLists()메서드 동작을 똑같이 따라해야 한다!!
-    @PostMapping("/makeReply/{todoId}")
+    @PostMapping("/makeReply")
     public ResponseEntity<?> createTodoReply(
-            @RequestBody TodoDTO todoDTO,
-            @PathVariable String todoId
+            @RequestBody TodoDTO todoDTO
     ){
         try{
             String tempUserId = "temporary-user"; //for TEST
-            String parentTodoId = todoId;
+            //String parentTodoId = todoId;
             //todoDTO.getReplies().size()-1 이 부분은 나중에 validation이 필요하다. todoDTO 내의 replies 리스트가 빈 리스트면 작동을 하지 않기 때문이다.
             String title = todoDTO.getReplies().get(todoDTO.getReplies().size()-1);
 
-            replyService.createReply(tempUserId, todoId, title);
+            replyService.createReply(tempUserId, todoDTO.getId(), title);
 
             return getTodoLists();
         }
@@ -121,5 +110,22 @@ public class TestTodoController {
         }
     }
 
+    //>>>>>>>>> Helper Methods Area <<<<<<<<<<<
+
+    private void extracted(List<TodoReplyEntity> replyEntities, List<TodoDTO> dtos) {
+        for(TodoDTO dto : dtos){
+            for(TodoReplyEntity todoReplyEntity : replyEntities){
+
+                log.info("Todo 아이디 : " + dto.getId());
+                log.info("TodoReply의 parentTodo 아이디 : " + todoReplyEntity.getParentTodoId());
+
+                if(dto.getId().equals(todoReplyEntity.getParentTodoId())){
+                    log.info("todoDTO에 리플라이 넣음");
+                    log.info(todoReplyEntity.getParentTodoId());
+                    dto.getReplies().add(todoReplyEntity.getTitle());
+                }
+            }//inner for
+        }//outer for
+    }
 
 }//end of class
